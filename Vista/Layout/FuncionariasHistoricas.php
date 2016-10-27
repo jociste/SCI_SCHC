dae3677f2c709c4ac8472f16a58f3c9135c93f4b
Joseline Cisternas <Joseline Cisternas@Joseline>
2016-10-26 21:29:08 -0400

    con opciones, falta darle funcionalidad

--- Vista/Layout/FuncionariasHistoricas.php
+++ Vista/Layout/FuncionariasHistoricas.php
@@ -15,45 +15,29 @@ $perfil = $_SESSION["idCargo"];
         <title>Sala cuna Hogar de Cristo</title>
         <!-- end: Meta -->
         <link id="page_favicon" href="../../Files/img/logo.png" rel="icon" type="image/x-icon" />
-        <!-- start: CSS -->
-        <link href="../../Files/Complementos/bootstrap/css/bootstrap-flat.css" rel="stylesheet">        
+       <link href="../../Files/Complementos/bootstrap/css/bootstrap-flat.css" rel="stylesheet">        
         <link href="../../Files/Complementos/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">        
         <link  href="../../Files/css/style.css" rel="stylesheet">
-
         <link  href="../../Files/css/chat.css" rel="stylesheet">  
         <link href="../../Files/css/chat.plugin.css" rel="stylesheet">
         <link  href="../../Files/js/charts/jquery.easy-pie-chart.css" rel="stylesheet">  
         <link  href="../../Files/Complementos/lib/selectize/selectize.css" rel="stylesheet">  
         <link  href="../../Files/css/component.css" rel="stylesheet"> 
         <link  href="../../Files/css/style-responsive.css" rel="stylesheet">
-
-
         <script src="../../Files/js/jquery.js"></script>
         <script src="../../Files/Complementos/lib/selectize/selectize.js"></script>        
         <script src="../../Files/Complementos/bootstrap/js/bootstrap.min.js"></script>  
-             
-        <script src="../../Files/Complementos/bootstrap/css/bootstrap.min.css"></script> 
-
         <script src="../../Files/js/charts/jquery.sparkline.min.js?v1.4.0"></script>
         <script src="../../Files/js/charts/jquery.easy-pie-chart.js?v1.4.0"></script>
         <script src="../../Files/js/charts/raphael.2.1.0.min.js?v1.4.0"></script>
         <script src="../../Files/js/charts/justgage.1.0.1.min.js?v1.4.0"></script>
-
         <script src="../../Files/Complementos/lib/scroll-slim/jquery.slimscroll.min.js"></script>
         <script src="../../Files/js/common.js"></script>
-
         <link rel="stylesheet" type="text/css" href="../../Files/Complementos/lib/jquery-easyui-1.4.2/themes/metro-green/easyui.css">
         <link rel="stylesheet" type="text/css" href="../../Files/Complementos/lib/jquery-easyui-1.4.2/themes/icon.css">
         <link rel="stylesheet" type="text/css" href="../../Files/Complementos/lib/jquery-easyui-1.4.2/demo/demo.css">
         <script src="../../Files/Complementos/lib/jquery-easyui-1.4.2/jquery.easyui.min.js"></script>
-
-        <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>-->
-
-        <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>-->
-        <!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"/>-->
-
         <link rel="stylesheet" type="text/css" href="../../Files/Complementos/lib/datatables/css/jquery.dataTables.css">
-
         <script type="text/javascript" charset="utf8" src="../../Files/Complementos/lib/datatables/jquery.dataTables.js"></script>
 
 
@@ -145,14 +129,11 @@ $perfil = $_SESSION["idCargo"];
                 </p>
             </footer>
         </div>
-    </div>
     <script src="../../Files/js/modernizr.custom.js"></script>
     <script src="../../Files/js/toucheffects.js"></script>
 
     <script>
                                     $(function () {
-                                        //$('#tablaFuncionarias').DataTable();
-                                        //$('#grid').DataTable();
                                         cargarFuncionarias();
                                     })
 
