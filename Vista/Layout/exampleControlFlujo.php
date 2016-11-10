1a88f412dfafe072582f0745f27cce6d7550a0f4
Lucrecia <Lucrecia@Lucrecia-PC>
2016-11-10 17:18:41 -0300

    Creando PDF reporte Version 1

---
+++ Vista/Layout/exampleControlFlujo.php
@@ -0,0 +1,150 @@
+
+<style type="text/css">
+    body{
+        //font-family: Calibri;
+        font-family:Arial, sans-serif;
+        font-size: 11px;
+        font-style: normal;
+        font-variant: normal;
+        font-weight: 400;
+        line-height: 11px;   
+    }
+    .center{
+        text-align: center;
+    }
+
+    .table {
+        //border-collapse: collapse;
+        //border: black 1px solid;  
+        width: 750px;
+        border-collapse:collapse;
+        border-spacing:0;
+        border-color:black;
+    }
+    .td-borde{
+        border-collapse: collapse;
+        border: black 1px solid;
+    }
+
+    .table td{
+        font-size:10px;
+        //padding:10px 5px;
+        border-style:solid;
+        border-width:1px;
+        overflow:hidden;
+        word-break:normal;
+        border-color:black;
+        color: black;
+        background-color:#fff;
+    }
+    //.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;border-color:#aaa;color:#fff;background-color:#f38630;}
+
+    .fondo{
+        background: #BDBDBD;
+    }
+
+</style>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <div class="center">
+            <h5>ANEXO 15<br>CONTROL FLUJOS DE EXISTENCIAS</h5>
+        </div>
+        <div>
+            <table class="table">
+                <tr><td class="td-borde" colspan="4">1.- IDENTIFICAR LA NATURALEZA DE LOS ARTÍCULOS DETALLADOS (marcar con una "X")</td></tr>
+                <tr><td class="td-borde fondo">a) Materiales Didácticos y de Enseñanza</td><td class="td-borde">X</td><td class="td-borde">b) Materiales de Oficina</td><td class="td-borde">X</td></tr>
+                <tr><td class="td-borde">c) Materiales y útiles de salud e higiene</td><td class="td-borde">X</td><td class="td-borde">d) Útiles de aseo (Utilizados para la limpieza del recinto)</td><td class="td-borde">X</td></tr>
+            </table>
+        </div>
+        <br>
+        <div>
+            <table class="table">
+                <tr><td class="td-borde" colspan="4">2.- IDENTIFICACIÓN DEL ESTABLECIMIENTO</td></tr>
+                <tr><td class="td-borde" colspan="3"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde" colspan="3">Nombre del Establecimiento</td><td class="td-borde">Código del Establecimiento</td></tr>
+                <tr><td class="td-borde" colspan="2"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde" colspan="2">Calle</td><td class="td-borde">N°</td><td class="td-borde">Comuna/Localidad</td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde" colspan="2"></td></tr>
+                <tr><td class="td-borde">Región</td><td class="td-borde">Teléfono</td><td class="td-borde" colspan="2">E-Mail</td></tr>
+            </table>
+        </div>
+        <br>
+        <div>
+            <table class="table">
+                <tr><td class="td-borde" colspan="4">3.- IDENTIFICACIÓN DE LA ENTIDAD ADMINISTRADORA</td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde">Nombre de la Enditdad</td><td class="td-borde">RUT</td><td class="td-borde">Provincia</td><td class="td-borde">Región</td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde">Nombre Representante Legal de la Entidad</td><td class="td-borde">RUT Representante Legal</td><td class="td-borde">Teléfono</td><td class="td-borde">E-Mail</td></tr>
+            </table>
+        </div>
+        <br>
+        <div>
+            <table class="table">
+                <tr><td class="td-borde">4.- DETALLE DE LAS FICHAS DE EXISTENCIAS</td></tr>
+            </table>
+        </div>
+        <br>
+        <div>
+            <table class="table">
+                <tr><td class="td-borde" colspan="4">DESCRIPCIÓN DEL ARTÍCULO:</td><td class="td-borde" colspan="6"></td></tr>
+                <tr><td class="td-borde" colspan="4">INGRESOS</td><td class="td-borde" colspan="4">RETIROS PARA SU USO</td><td class="td-borde" rowspan="2">SALDO</td><td class="td-borde">DE USO EXCLUSIVO DE JUNJI</td></tr>
+                <tr><td class="td-borde">N° Factura o Boleta</td><td class="td-borde">Fecha (dd/mm/aa)</td><td class="td-borde">Proveedor</td><td class="td-borde">Cantidad Ingresada</td><td class="td-borde">Fecha del Retiro (dd/mm/aa)</td><td class="td-borde">Nombre y firma de persona que retira</td><td class="td-borde">Cantidad retirada</td><td class="td-borde">Destino</td><td class="td-borde">V° 8°Del Fiscalizador (en el último saldo a la fecha de la fiscalización)</td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td><td class="td-borde"></td></tr>
+            </table>
+        </div>
+        <br>
+        <div>
+            <table class="table">
+                <tr><td class="td-borde">5.- OBSERVACIONES DE LA DIRECTORA DEL ESTABLECIMIENTO Y/O DEL ENCARGADO DEL CONTROL DE LAS EXISTENCIAS (indicar fechas de revisión y/o de cuadraturas) y sus respectivas observaciones.</td></tr>
+                <tr><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td></tr>
+                <tr><td class="td-borde"></td></tr>
+            </table>
+        </div>
+        <br>
+        <div>
+            <table class="table">
+                <tr><td class="td-borde" colspan="3">6.- DE USO EXCLUSIVO DE LA DIRECCIÓN REGIONAL DE LA JUNJI (a la fecha de la fiscalización)</td></tr>
+                <tr><td class="td-borde">FECHA FISCALIZACIÓN</td><td class="td-borde" colspan="2"></td></tr>
+                <tr><td class="td-borde" colspan="3">OBSERVACIONES:</td></tr>
+                <tr><td class="td-borde" colspan="3"></td></tr>
+                <tr><td class="td-borde" colspan="3"></td></tr>
+                <tr><td class="td-borde" colspan="3"></td></tr>
+                <tr><td class="td-borde" colspan="3"></td></tr>
+                <tr><td class="td-borde" colspan="3"></td></tr>
+                <tr><td class="td-borde" colspan="3"></td></tr>
+                <tr><td class="td-borde" colspan="3"></td></tr>
+                <tr><td class="td-borde" colspan="3"></td></tr>
+                <tr><td class="td-borde" colspan="3"></td></tr>
+                <tr><td class="td-borde" colspan="2"></td><td class="td-borde"></td></tr>
+                <tr><td class="td-borde" colspan="2">7.- NOMBRE, FRIMA Y TIMBRE DE LA DIRECTORA DEL ESTABLECIMIENTO</td><td class="td-borde">NOMBRE Y FIRMA DE LA ENCARGADA(O) DE LA BODEGA (en el caso de corresponder)</td></tr>
+            </table>
+        </div>
+    </body>
+</html>
